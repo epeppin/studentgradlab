d431029722805f74524a9b3086f415e564288cd9
epeppin <epeppin@my.wctc.edu>
2020-12-02 21:02:59 -0600

    Update to objects

--- script.js
+++ script.js
@@ -1,51 +1,88 @@
 $(document).ready(
     function () {
         // add event handlers
-        $("form").submit(scoreAssign)
+        $("button#submit").click(scoreAssign)
+        $("button#sortName").click(function (event){
+            event.preventDefault();
+            students.sort(sortName)
+            var output = "";
+            displayStudents();
+        })
+        $("button#sortPerc").click(function (event){
+            event.preventDefault();
+            students.sort(sortPercent)
+            displayStudents();
+        })
 
-        var entry = {
-            firstName: "",
-            lastName: "",
-            pointPos: 0,
-            pointsEarn: 0
-        };
+        var students = [];
+
 
         // all functions (program logic)
         function scoreAssign (event){
             event.preventDefault();
 
-            var outputObj = {};
+            var outputObj = {
+                gradeText: "Grade: "
+            };
 
-            entry.firstName = $("#firstName").val()
-            entry.lastName = $("#lastName").val()
-            entry.pointPos = $("#pointsPos").val()
-            entry.pointsEarn = $("#pointsEarn").val()
+            outputObj.firstName = $("#firstName").val()
+            outputObj.lastName = $("#lastName").val()
+            outputObj.pointPos = $("#pointsPos").val()
+            outputObj.pointsEarn = $("#pointsEarn").val()
 
-            outputObj.percent = entry.pointsEarn / entry.pointPos * 100
+            outputObj.percent = Math.round(outputObj.pointsEarn / outputObj.pointPos * 100)
             outputObj.percentText = outputObj.percent + "%"
 
 
 
             if (outputObj.percent > 95){
-                $("#outputGrade").text("A")
+                outputObj.grade = "A"
             }else if (outputObj.percent > 89.99){
-                $("#outputGrade").text("A-")
+                outputObj.grade = "A-"
             }else if (outputObj.percent > 84.99){
-                $("#outputGrade").text("B")
+                outputObj.grade = "B"
             }else if (outputObj.percent > 79.99){
-                $("#outputGrade").text("B-")
+                outputObj.grade = "B-"
             }else if (outputObj.percent > 74.99){
-                $("#outputGrade").text("C")
+                outputObj.grade = "C"
             }else {
-                $("#outputGrade").text("F")
+                outputObj.grade = "F"
             }
-            $("#grade").show()
-            $("#percent").show()
+
+            students.push(outputObj)
+
+            displayStudents();
+        }
+
+        function displayStudents() {
+            var output = "";
+            for (var studentItem of students){
+                output += (`${studentItem.firstName} ${studentItem.lastName}   ${studentItem.gradeText}${studentItem.grade}  
+                ${studentItem.percentText}`)
+                output += '<br>'
+            }
+            $("#outputFirst").html(output)
             $("#first").show()
-            $("#last").show()
-            $("#outputFirst").text(entry.firstName)
-            $("#outputLast").text(entry.lastName)
-            $("#outputPercent").text(outputObj.percentText)
+        }
+
+        function sortName (name1, name2){
+
+            if (name1.lastName < name2.lastName)
+                return -1;
+            else if (name1.lastName > name2.lastName)
+                return  1;
+            else
+                return 0;
+
+        }
+        function sortPercent (perc1, perc2){
+
+            if (perc1.percent > perc2.percent)
+                return -1;
+            else if (perc1.percent < perc2.percent)
+                return  1;
+            else
+                return 0;
 
         }
 
